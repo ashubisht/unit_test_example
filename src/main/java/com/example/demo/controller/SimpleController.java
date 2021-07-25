0dbd895b8b781934232d161566d90d887aeb1a28
Utkarsh Bisht <utkarsh.bisht7@gmail.com>
2021-07-26 02:44:39 +0530

    Add static initialiser block

--- src/main/java/com/example/demo/controller/SimpleController.java
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/simple")
 public class SimpleController {
 
+    // This block is even more bad. Like kinda super bad. Even this I know :D
+    static{
+        System.out.println("This is static initializer block");
+    }
+
     // THIS BELOW BLOCK IS BAD. I KNOW!
     {
-        System.out.printf("This is an initilizer block. Can we suppress this?" + System.lineSeparator() +
+        System.out.printf("This is an initializer block. Can we suppress this?" + System.lineSeparator() +
                 "What if this app connects to external components in this block" + System.lineSeparator()) ;
     }
 
